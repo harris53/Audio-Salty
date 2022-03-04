3ecf6a459e9f1c8d0275ba527ab54d9e17146968
harris53 <90465398+harris53@users.noreply.github.com>
2022-03-04 08:53:25 -0500

    Updated analysis function and removed comments
    Analysis function now uses the sigmoid function. Deleted comments for previous calculations. Removed commented print statements.

--- main.py
+++ main.py
@@ -5,24 +5,15 @@ import matplotlib.pyplot as plt
 
 # returns rage number
 def anylasis(number):
-    '''
-    rage = 1 / (1 + (np.e ** (number * -1)))
-    rage = rage * 1000
-    '''
 
-    rage = number / 60
-    # rage = number
+    rage = 1 / (1 + (np.e ** ((number/100000) * -1)))
+    rage = rage * 1000
 
     return rage
 
-
-
 def output(text):
-    # Use a breakpoint in the code line below to debug your script.
-    print(text)  # Press Ctrl+F8 to toggle the breakpoint.
+    print(text)
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     wav = wave.open('sample.wav', 'r')
 
@@ -37,7 +28,7 @@ if __name__ == '__main__':
         sys.exit(0)
     '''
 
-    print("sample.wav is : ")
+    print("processing sample.wav")
     tempRage = anylasis(raw[0])
     totalRage = 0
     values = 0
@@ -45,17 +36,14 @@ if __name__ == '__main__':
         if anylasis(i) > tempRage:
             tempRage = anylasis(i)
         if i >= 0:
-            #print(i, end=' ')
             totalRage = totalRage + anylasis(i)
             values = values + 1
         else:
-            #print(-i, end=' ')
             totalRage = totalRage - anylasis(i)
             values = values + 1
 
     maxRage = tempRage
     avgRage = totalRage / values
-    #print(maxRage)
 
     Time = np.linspace(0,len(raw) / sampleRate, num=len(raw))
 
@@ -64,15 +52,7 @@ if __name__ == '__main__':
     plt.ylabel("Rage")
     plt.show()
 
-
-
-
     wav.close()
 
 
-# maxium rage = 1000
-
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# maximum rage = 1000
